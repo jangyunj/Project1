196cf497544122489d6d04ef58612aa23a493a68
Yun Jang <jang.yunj7@gmail.com>
2023-03-12 20:57:40 -0400

    data types

---
+++ Project1/Project1.cpp
@@ -0,0 +1,19 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//int main() {
+//	int number{}; //empty brace initializer (same as int number = 0;) 
+//	cout << number;
+//}
+
+int main() {
+	int number = 0b11111111; //binary = 255
+	int number = 0xff; //hexadecimal = 255
+	cout << number;
+
+}
